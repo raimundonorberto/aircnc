46b721bbd2a6a0c376aca0563ddecac706fc8e62
Raimundo Lameira <raimundonorberto@gmail.com>
2019-10-20 16:44:04 -0300

    Código desenvolvido durante a semana Omnistack 9.

---
+++ backend/src/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const routes = require('./routes');
+const cors = require('cors');
+const path = require('path');
+
+const socketio = require('socket.io');
+const http = require('http');
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+    return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+
+server.listen(3333);
